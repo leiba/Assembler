68d1b635b147d2fcaaa70a3f2c972c5617ea50fd
leibaWorld@gmail.com <leibaWorld@gmail.com>
2016-05-28 12:27:25 +0300

    master // assembler

--- c/Arduino/Arduino/Arduino.c
+++ c/Arduino/Arduino/Arduino.c
@@ -4,35 +4,43 @@
 
 #define F_CPU 20000000
 
-int a = 0, b = 0;
-
-ISR (TIMER0_COMPA_vect) {
-	if (a++ > 100) {
-		PORTB ^= (1 << PORTB0);
-		a = 0;
-	}
-}
-	
-ISR (TIMER0_COMPB_vect) {
-	if (b++ > 50) {
-		PORTB ^= (1 << PORTB1);
-		b = 0;
-	}	
-}
+double pwm = 0;
 
 int main(void)
 {
-	DDRB = 0xFF;
-	
 	sei();
-	TCCR0A = (1 << WGM01);
-	TCCR0B = (1 << CS02) | (1 << CS00);
 	
-	OCR0A  = 195;
-	OCR0B  = 195; 
+	DDRD = (1 << PORTD6) | (1 << PORTD5);
 	
-	TIMSK0 = (1 << OCIE0A) | (1 << OCIE0B);	
+	// Compare output mode, Fast PWM mode. Режим работы ШИМ
+	// Очистка счетчика, установка уровня в 0 при совпадении
+	TCCR0A = (1 << COM0A1); // Clear on compare match, set bottom OCR0A
+	TCCR0A |= (1 << COM0B1); // Clear on compare match, set bottom OCR0B
+	
+	// Waveform generation mode. Режим работы таймера
+	TCCR0A |= (1 << WGM01) | (1 << WGM00); // Fast PWM
+	
+	// Clock select bit. Предделитель
+	TCCR0B = (1 << CS00); // Предделитель 1
+	
+	// Output compare register
+	// Регистр сравнения
+	OCR0A = OCR0B = 0; // Всегда в 0. ШИМ уровень 5v, заканчивается сразу счета
+	
+	// Timer/Counter Interrupt mask register
+    // Разрешение прерывания
+	TIMSK0 = (1 << TOIE0); // Прерывание по переполнению счетчика
 
-	while (1) {}	
+	while (1) {
+		_delay_ms(10);
+		
+		if ((pwm += 1) > 100) {
+			pwm = 0;
+		}
+	}	
+}
+
+ISR(TIMER0_OVF_vect) {
+	OCR0A = OCR0B = (pwm / 100) * 255;	
 }
 
--- c/Practice/Practice/learn.c
+++ c/Practice/Practice/learn.c
@@ -316,6 +316,57 @@ ISR (TIMER0_COMPB_vect) {
 }
 
 /*
+ Fast PWM (OCnA, OCnB)
+*/
+#include <avr/interrupt.h>
+
+double pwm = 0;
+
+int main(void)
+{
+    // Разрешаем глобальные прерывания
+    sei();
+	
+    // Устанавливаем порты OCR0A и OCR0B, для вывода ШИМ
+    DDRD = (1 << PORTD6) | (1 << PORTD5);
+	
+    // Compare output mode, Fast PWM mode. Режим работы ШИМ
+    // Очистка счетчика, установка уровня в 0 при совпадении
+    TCCR0A = (1 << COM0A1); // Clear on compare match, set bottom OCR0A
+    TCCR0A |= (1 << COM0B1); // Clear on compare match, set bottom OCR0B
+	
+    // Waveform generation mode. Режим работы таймера
+    TCCR0A |= (1 << WGM01) | (1 << WGM00); // Fast PWM
+	
+    // Clock select bit. Предделитель
+    TCCR0B = (1 << CS00); // Предделитель 1
+	
+    // Output compare register
+    // Регистр сравнения
+    OCR0A = OCR0B = 0; // Счетчик сравнения. При 0, Нулевая ширина импулься 5v
+	
+    // Timer/Counter Interrupt mask register
+    // Разрешение прерывания
+    TIMSK0 = (1 << TOIE0); // Прерывание по переполнению счетчика
+
+    while (1) {
+        _delay_ms(10);
+		
+        // Увеличиваем ширину ШИМ импульсов
+        if ((pwm += 1) > 100) {
+            pwm = 0;
+        }
+    }	
+}
+
+// Обработка прерываения переполнения счетчика
+ISR(TIMER0_OVF_vect) {
+    // Устанавливаем сравнение для счетчика
+    // Сдвигая счетчик, увеличивается длина импульса 5v
+    OCR0A = OCR0B = (pwm / 100) * 255;
+}
+
+/*
  Внешние прерывания (INT0, INTn)
  
  Datasheet -> External Interrupts
